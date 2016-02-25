0a6bf1721d6c4d07836952cf68c5b828a30cd1f2
klesken <klesken91@gmail.com>
2016-02-25 21:01:54 +0100

    Solved problem 3 and cleaned up some code
    Cleanerish

--- Problem3.java
+++ Problem3.java
@@ -3,8 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
 
+What is the largest prime factor of the number 600851475143 ?
+
+*/
 package projecteulerproblems;
+
+import java.math.BigInteger;
 
 /**
  *
@@ -26,6 +33,16 @@ public class Problem3 implements ProjectEulerProblem{
     @Override
     public void solve() {
         UtilityClass.startTimer();
+        
+        int maxPrime = 10000000 - 1;
+        int[] primes = UtilityClass.getPrimes(maxPrime);
+        BigInteger number = new BigInteger("600851475143");
+        for(int i = primes.length-1; i >= 0; i--){
+            if(number.remainder(BigInteger.valueOf(primes[i])) == BigInteger.ZERO){
+                solution = primes[i];
+                break;               
+            }
+        }
         
         
         UtilityClass.endTimer(this);
